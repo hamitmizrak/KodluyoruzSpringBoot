e4beaa0b2a8ced721066386a5ecdf48252e7dec6
hamitmizrak <hamitmizrak@gmail.com>
2022-03-15 19:14:28 +0300

    DevTools Aktif etmek

---
+++ src/main/java/com/hamitmizrak/utils/ConfigProgramming.java
@@ -0,0 +1,8 @@
+package com.hamitmizrak.utils;
+
+public class ConfigProgramming {
+
+    //DevTools ==> LiveReload
+    //file ==> settings ==> compiler ==> Build project automatically(OK clicked) (Ctrl+Alt+S)
+   // file ==> Advanced Settings ==>Allow Auto-make to start even (OK clicked)
+}
