9217ca8f129911fca6e5a4393a48d9682fda7f93
hamitmizrak <hamitmizrak@gmail.com>
2022-03-15 19:14:13 +0300

    kısayollar

---
+++ src/main/java/com/hamitmizrak/utils/IntellijShortCut.java
@@ -0,0 +1,9 @@
+package com.hamitmizrak.utils;
+
+public class IntellijShortCut {
+//    Setting ==> ctrl+alt+s
+//    Alt+Ins ==> constructor, toString ,getter setter
+//    Ctrl+/ => single comment
+//    Ctrl+shift+/ => multiple comment
+
+}
