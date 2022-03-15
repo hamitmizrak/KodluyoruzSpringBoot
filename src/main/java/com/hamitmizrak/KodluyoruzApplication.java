56e3fc6219e470c43173b77a96e12a47894776d3
hamitmizrak <hamitmizrak@gmail.com>
2022-03-15 18:34:28 +0300

    first commit ( Kodluyoruz)

---
+++ src/main/java/com/hamitmizrak/KodluyoruzApplication.java
@@ -0,0 +1,13 @@
+package com.hamitmizrak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodluyoruzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodluyoruzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hamitmizrak/KodluyoruzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamitmizrak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodluyoruzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
