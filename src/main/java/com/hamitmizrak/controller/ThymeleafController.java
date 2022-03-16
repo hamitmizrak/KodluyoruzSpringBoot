690b257493510c3c49f86a8f7c2f3838c340b925
hamitmizrak <hamitmizrak@gmail.com>
2022-03-16 11:34:47 +0300

    thymeleaf css,html,model

---
+++ src/main/java/com/hamitmizrak/controller/ThymeleafController.java
@@ -0,0 +1,34 @@
+package com.hamitmizrak.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ThymeleafController {
+
+    //http://localhost:8080/thymeleaf1
+    @GetMapping("/thymeleaf1")
+    // @ResponseBody
+    public String getThymeleaf1(){
+        return "thymeleaf1";
+    }
+
+
+    //http://localhost:8080/thymeleaf2
+    @GetMapping("/thymeleaf2")
+    public String getThymeleaf2Model(Model model){
+        model.addAttribute("key_model1","Ben Modelden geldim-1");
+        model.addAttribute("key_model2","Ben Modelden geldim-2");
+        return "thymeleaf1";
+    }
+
+    //http://localhost:8080/thymeleaf3
+    @GetMapping("/thymeleaf3")
+    public String getThymeleaf3Model(Model model){
+        model.addAttribute("key_model1","Ben Modelden geldim-1");
+        model.addAttribute("key_model2","Ben Modelden geldim-2");
+        return "tyhmeleaf_file/thymeleaf3";
+    }
+}
---
+++ src/main/resources/static/js/template.js
@@ -0,0 +1,1 @@
+alert("MErhabalar JavaScript");
