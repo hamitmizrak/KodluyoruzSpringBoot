0be6e3ea0dae7fe403f0224a97847d2fafca2869
hamitmizrak <hamitmizrak@gmail.com>
2022-03-16 16:55:52 +0300

    ModelMapperBean

---
+++ src/main/java/com/hamitmizrak/bean/ModelMapperBean.java
@@ -0,0 +1,16 @@
+package com.hamitmizrak.bean;
+
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperBean {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+
+}
