8eb20c897d2528a705612a2ad25f97d565be1625
hamitmizrak <hamitmizrak@gmail.com>
2022-03-16 13:05:12 +0300

    @PathVariable, name,required

--- src/main/java/com/hamitmizrak/controller/ThymeleafController.java
+++ src/main/java/com/hamitmizrak/controller/ThymeleafController.java
@@ -4,6 +4,7 @@ import com.hamitmizrak.dto.ProductDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
@@ -12,6 +13,16 @@ import java.util.List;
 @Controller
 public class ThymeleafController {
     //Ctrl+alt+L=formmater
+
+
+    //option: root {}
+    //http://localhost:8080
+    @GetMapping({"/","index" })
+    public String index(){
+        return "index";
+    }
+
+    //////////////////////////////////////////////////////////////////////////
 
     //@ResponseBody
     //http://localhost:8080/thymeleaf1
@@ -76,6 +87,20 @@ public class ThymeleafController {
         model.addAttribute("product_liste", listem);
         return "thymeleaf6";
     }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Model Object göndermek
+    //http://localhost:8080/thymeleaf7/4
+    @GetMapping( {"/thymeleaf7","/thymeleaf7/{id}"} )
+    public String getThymeleaf7ModelObject(Model model, @PathVariable(name = "id",required = false) Long id) {
+        if(id!=null){
+            model.addAttribute("key_model1", "id: "+id);
+        }else{
+            model.addAttribute("key_model1", "id bulunmadı");
+        }
+        return "thymeleaf7";
+    }
+
 
 
 }
