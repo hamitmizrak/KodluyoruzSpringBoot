98e4ca5624f2db69d176051780e791709d7eee1b
hamitmizrak <hamitmizrak@gmail.com>
2022-03-18 11:43:35 +0300

    PermaLink

--- src/main/java/com/hamitmizrak/ui/mvc/utils/PermaLink.java
+++ src/main/java/com/hamitmizrak/ui/mvc/utils/PermaLink.java
@@ -5,4 +5,5 @@ public class PermaLink {
     //  SWAGGER: http://localhost:8080/swagger-ui/index.html
     //  H2-console: http://localhost:8080/h2-console/
     //  JSON Online parser:  https://jsoneditoronline.org/#left=local.gesomi&right=local.revibe
+    //  Jwt ==> https://jwt.io/
 }
